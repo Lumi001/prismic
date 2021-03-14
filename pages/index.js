54c8b4444b1159af7b2d2a55536390d0e8a4a2ce
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2021-03-14 14:11:41 +0100

    change title to TruCSR

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home({ footer, card, carousel, connect_with_us, navigati
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>TruCSR</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=Ubuntu:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet" />
