7edce818265b6a164afd15f25abe33d1582aa53e
t4christ <bakaretemitayo712@gmail.com>
2021-03-16 21:31:12 +0100

    create dev-pipeline

--- docker_setup.sh
+++ docker_setup.sh
@@ -8,9 +8,14 @@ if [ $? -eq 0 ]; then
    echo Updated Services Successfully
 
    else
-   echo Fresh Deployment
-   sudo docker service create -p 8009:5500 --name prismic_frontend_public 127.0.0.1:5500/prismic_frontend_public:$1 
-  
+    echo Fresh Deployment
+    sudo docker service create -p 5500:5500 --name prismic_frontend_public 127.0.0.1:5500/prismic_frontend_public:$1 
+    if [ $? -eq 0 ]; then
+    echo "First Deployment Successful"
+    else
+    exit 1
+    echo "First Deployment Failed"
+    fi
    fi
 else
    exit 1
