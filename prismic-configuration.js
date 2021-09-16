1114aeb18767058215cbea82d4820b08a423a1f7
Nicholas <nirvanaklaus@gmail.com>
2021-09-16 12:46:05 +0100

    temporary test

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -16,7 +16,44 @@ export const Client = (req = null) => {
             { accessToken: prismicAccessToken } :
             {}
 
-            return { ...reqOption, ...accessTokenOption }
-        }
-        return Prismic.client(apiEndpoint, options(req, accessToken));
+        return { ...reqOption, ...accessTokenOption }
     }
+    return Prismic.client(apiEndpoint, options(req, accessToken));
+}
+
+export const PRISMIC_heading = (item, index) => {
+    return item?.heading[index = 0]?.text || " "
+}
+export const PRISMIC_title = (item, index) => {
+    return item?.title[index = 0]?.text || " "
+}
+export const PRISMIC_heading2 = (item, index) => {
+    return item?.heading2[index = 0]?.text || " "
+}
+export const PRISMIC_name = (item, index) => {
+    return item?.name[index = 0]?.text || " "
+}
+export const PRISMIC_text = (item, index) => {
+    return item?.text[index = 0]?.text || " "
+}
+export const PRISMIC_text2 = (item, index) => {
+    return item?.text[index = 1]?.text || " "
+}
+export const PRISMIC_message = (item, index) => {
+    return item?.message[index = 0]?.text || " "
+}
+export const PRISMIC_link_text = (item, index) => {
+    return item?.link_text || " "
+}
+export const PRISMIC_role = (item, index) => {
+    return item?.role || " "
+}
+export const PRISMIC_link = (item, index) => {
+    return item?.link?.url || " "
+}
+export const PRISMIC_image = (item, image) => {
+    return item?.image?.url || image || "#ffffff "
+}
+export const PRISMIC_icon = (item) => {
+    return item?.icon?.url || " "
+}
